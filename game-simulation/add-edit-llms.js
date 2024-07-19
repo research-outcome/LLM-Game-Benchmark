3d35982d61eb87d35114928e57880c900d5930fb
Colby Edell <129539668+colbyedell@users.noreply.github.com>
2024-07-19 19:49:46 -0400

    Added gpt-4o-mini.

--- game-simulation/add-edit-llms.js
+++ game-simulation/add-edit-llms.js
@@ -111,6 +111,7 @@ function modelSupportsText(modelName) {
 // If the current model name supports images, return true.
 function modelSupportsImages(modelName) {
     return modelName === "gpt-4o" ||
+        modelName === "gpt-4o-mini" ||
         modelName === "gpt-4-turbo" ||
         modelName === "gemini-1.5-pro" ||
         modelName === "gemini-1.5-flash" ||
@@ -160,6 +161,7 @@ export function updateAddModelFields(event) {
                 "<option value=\"gpt-3.5-turbo\">gpt-3.5-turbo</option>" +
                 "<option value=\"gpt-4\">gpt-4</option>" +
                 "<option value=\"gpt-4o\">gpt-4o</option>" +
+                "<option value=\"gpt-4o-mini\">gpt-4o-mini</option>" +
                 "<option value=\"gpt-4-turbo\">gpt-4-turbo</option>" +
             "</select>";
         document.getElementById("llm-name-container").style.display = "inline";
